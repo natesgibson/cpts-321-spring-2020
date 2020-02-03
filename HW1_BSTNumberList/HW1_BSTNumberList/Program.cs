d4c7efbc2388dd75c8aa370ed2642f9ae4f0a5bd
Nate <nathanaelsgibson@gmail.com>
2020-02-02 16:48:24 -0800

    Fixed about 20 warnings, mostly to do with commenting. Added GlobalSuppressions.cs file.

--- HW1_BSTNumberList/HW1_BSTNumberList/Program.cs
+++ HW1_BSTNumberList/HW1_BSTNumberList/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Nate Gibson">
+//     Copyright (c) Nate Gibson. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +12,18 @@ using System.Threading.Tasks;
 
 namespace HW1_BSTNumberList
 {
+    /// <summary>
+    /// Class contains the main method for BST Number List C# console application.
+    /// </summary>
     public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// This is the main method for the BST Number List C# console application.
+        /// Users are asked to input a string of integers, separated by spaces.
+        /// Program puts the integers into a BST and outputs certain statistics about the BST.
+        /// </summary>
+        /// <param name="args">Arguments passed in at start of program.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
--- HW1_BSTNumberList/HW1_BSTNumberListTests/TestClass.cs
+++ HW1_BSTNumberList/HW1_BSTNumberListTests/TestClass.cs
@@ -1,15 +1,27 @@
-﻿// NUnit 3 tests
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestClass.cs" company="Nate Gibson">
+//     Copyright (c) Nate Gibson. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 
-
-namespace HW1_BSTNumberList
+namespace HW1_BSTNumberListTests
 {
+    /// <summary>
+    /// Contains tests for HW1_BSTNumberList Program.
+    /// </summary>
     [TestFixture]
     public class TestClass
     {
+        /// <summary>
+        /// Example test.
+        /// </summary>
         [Test]
         public void TestMethod()
         {
