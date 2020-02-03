5b7a0fdefac08f8e3f54d2746c835882abfe3b2b
Nate <nathanaelsgibson@gmail.com>
2020-02-03 10:06:00 -0800

    suppressed rule about public methods being above private to have a private helper method immediately below a public method

--- HW1_BSTNumberList/HW1_BSTNumberList/GlobalSuppressions.cs
+++ HW1_BSTNumberList/HW1_BSTNumberList/GlobalSuppressions.cs
@@ -3,4 +3,5 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "I  want some headers to not have the standard copyright format, such as this file and AssemblyInfo.cs")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "I  want some headers to not have the standard copyright format, such as this file and AssemblyInfo.cs.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements should be ordered by access", Justification = "I want to have a private (recursive) helper function immediately below a public function.", Scope = "member", Target = "~M:HW1_BSTNumberList.IntBST.GetOrderedTree~System.String")]
