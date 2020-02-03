2a2e49b0e804862a9ce30cc2960e8849c75813bd
Nate <nathanaelsgibson@gmail.com>
2020-02-03 10:34:30 -0800

    previous commit message incorrect. should have said 'fixed TestBSTAddDuplicate error'. This commit modifies suppression to have wider scope

--- HW1_BSTNumberList/HW1_BSTNumberList/GlobalSuppressions.cs
+++ HW1_BSTNumberList/HW1_BSTNumberList/GlobalSuppressions.cs
@@ -4,4 +4,4 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "I  want some headers to not have the standard copyright format, such as this file and AssemblyInfo.cs.")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements should be ordered by access", Justification = "I want to have a private (recursive) helper function immediately below a public function.", Scope = "member", Target = "~M:HW1_BSTNumberList.IntBST.GetOrderedTree~System.String")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements should be ordered by access", Justification = "I want to have private helper functions immediately below public functions.")]
