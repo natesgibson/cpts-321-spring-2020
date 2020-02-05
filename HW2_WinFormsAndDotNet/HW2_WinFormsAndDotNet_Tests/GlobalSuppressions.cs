0f84180a11ca69538f9108f4983549faf2191a05
Nate <nathanaelsgibson@gmail.com>
2020-02-04 22:18:04 -0800

    added new VS WinForums project and NUnit test project. Configured stylecop stuff.

---
+++ HW2_WinFormsAndDotNet/HW2_WinFormsAndDotNet_Tests/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1512:Single-line comments should not be followed by blank line", Justification = "I want to have an independent single-line comment.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "I  want some headers to not have the standard copyright format, such as this file and AssemblyInfo.cs")]
