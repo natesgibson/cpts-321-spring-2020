aafe5e37931727fc73ff3381a82fd8db1b8e7066
Nate <nathanaelsgibson@gmail.com>
2020-02-05 11:05:28 -0800

    set up form with text box according to assignment instructions

--- HW2_WinFormsAndDotNet/HW2_WinFormsAndDotNet/Form1.cs
+++ HW2_WinFormsAndDotNet/HW2_WinFormsAndDotNet/Form1.cs
@@ -16,5 +16,10 @@ namespace HW2_WinFormsAndDotNet
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
