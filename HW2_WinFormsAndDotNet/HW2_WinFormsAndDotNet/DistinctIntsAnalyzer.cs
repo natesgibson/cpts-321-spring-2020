c66add0495b37f4f5b59c03d558cca90b9368e26
Nate <nathanaelsgibson@gmail.com>
2020-02-07 11:56:27 -0800

    Added skeleton code for DistinctIntsAnalyzer class which contains the three different methods to find the distinct number of elements in an int list

---
+++ HW2_WinFormsAndDotNet/HW2_WinFormsAndDotNet/DistinctIntsAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="DistinctIntsAnalyzer.cs" company="Nate Gibson">
+// Copyright (c) Nate Gibson. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_WinFormsAndDotNet
+{
+    /// <summary>
+    /// Contains 3 different methods to determine the number of unique items in an int list.
+    /// </summary>
+    public class DistinctIntsAnalyzer
+    {
+        /// <summary>
+        /// Returns the number of distinct items in an int list
+        /// using the hash set method to find distinct items.
+        /// </summary>
+        /// <param name="list">Int list.</param>
+        /// <returns>The number of distinct items in {list}.</returns>
+        public int HashMethGetNumDistinct(List<int> list)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the number of distinct items in an int list
+        /// using the O(1) storage complexity method to find distinct items.
+        /// </summary>
+        /// <param name="list">Int list.</param>
+        /// <returns>The number of distinct items in {list}.</returns>
+        public int BigO1MethGetNumDistinct(List<int> list)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the number of distinct items in an int list
+        /// using the sorted method to find distinct items.
+        /// </summary>
+        /// <param name="list">Int list.</param>
+        /// <returns>The number of distinct items in {list}.</returns>
+        public int SortedMethGetNumDistinct(List<int> list)
+        {
+            return 0;
+        }
+    }
+}
