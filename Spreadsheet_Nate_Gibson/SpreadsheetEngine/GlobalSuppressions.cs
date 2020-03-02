c5fb03088030c85450c7a43c87dd710c1fd262d1
Nate <nathanaelsgibson@gmail.com>
2020-03-01 18:48:38 -0800

    Added SpreadsheetEngine class library project

---
+++ Spreadsheet_Nate_Gibson/SpreadsheetEngine/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "I want to have a custom header with my name and wsu id.")]
