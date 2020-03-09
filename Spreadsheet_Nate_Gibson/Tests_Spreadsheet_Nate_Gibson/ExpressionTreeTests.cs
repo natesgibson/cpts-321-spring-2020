9d03c4e7722e8f1b559d1cc7fe58d386a6d5d7ae
Nate <nathanaelsgibson@gmail.com>
2020-03-08 20:48:39 -0700

    Refactored existing tests for spredsheet and expression tree into seperate class files and added name and WSU ID to the top of all modified files which lacked it.

--- Spreadsheet_Nate_Gibson/Tests_Spreadsheet_Nate_Gibson/ExpressionTreeTests.cs
+++ Spreadsheet_Nate_Gibson/Tests_Spreadsheet_Nate_Gibson/ExpressionTreeTests.cs
@@ -12,78 +12,60 @@ using SpreadsheetEngine;
 namespace Tests_Spreadsheet_Nate_Gibson
 {
     /// <summary>
-    /// Tests for spreadsheet project.
+    /// Tests for Expression Tree project.
     /// </summary>
     [TestFixture]
-    public class TestClass
+    public class ExpressionTreeTests
     {
         //---------------------------------------------------------------
-        //                     Spreadsheet  Tests:
+        //                      SetVariable Tests:
         //---------------------------------------------------------------
 
         /// <summary>
-        /// Normal case for Spreadsheet GetCell method.
+        /// CONDITION: Expression Tree Evaluate method works.
+        /// Tests a normal case for expression tree set variable method.
         /// </summary>
         [Test]
-        public void TestSpreadsheetGetCellNormal()
+        public void TestSetVariableNormal()
         {
-            Spreadsheet s = new Spreadsheet(5, 5);
-            SpreadsheetCell newCell = new SpreadsheetCell(0, 0);
-            Assert.That(s.GetCell(4, 0).Value, Is.EqualTo(newCell.Value), "Spreadsheet returned undexpected cell at index.");
+            string expression = "A+1";
+            ExpressionTree tree = new ExpressionTree(expression);
+            tree.SetVariable("A", 3);
+
+            Assert.That(tree.Evaluate(), Is.EqualTo(4.0), "Evaluate retuned unexpected value.");
         }
 
         /// <summary>
-        /// Nonexistent case for Spreadsheet GetCell method.
+        /// CONDITION: Expression Tree Evaluate method works.
+        /// Tests a multiple variables case for expression tree set variable method.
         /// </summary>
         [Test]
-        public void TestSpreadsheetGetCellNonexistent()
+        public void TestSetVariableMultiple()
         {
-            Spreadsheet s = new Spreadsheet(5, 5);
-            Assert.That(s.GetCell(6, 0), Is.EqualTo(null), "Spreadsheet returned undexpected cell at index.");
+            string expression = "A+1+B";
+            ExpressionTree tree = new ExpressionTree(expression);
+            tree.SetVariable("A", 3);
+            tree.SetVariable("B", 5);
+
+            Assert.That(tree.Evaluate(), Is.EqualTo(9.0), "Evaluate retuned unexpected value.");
         }
 
         /// <summary>
-        /// Normal case for Spreadsheet ColumnCount method.
+        /// CONDITION: Expression Tree Evaluate method works.
+        /// Tests a long variable name case for expression tree set variable method.
         /// </summary>
         [Test]
-        public void TestSpreadsheetColumnCountNormal()
+        public void TestSetVariableLongName()
         {
-            Spreadsheet s = new Spreadsheet(6, 5);
-            Assert.That(s.ColumnCount(), Is.EqualTo(5), "Spreadsheet returned unexpected column count.");
-        }
+            string expression = "IAmThree+1";
+            ExpressionTree tree = new ExpressionTree(expression);
+            tree.SetVariable("IAmThree", 3);
 
-        /// <summary>
-        /// Zero case for Spreadsheet ColumnCount method.
-        /// </summary>
-        [Test]
-        public void TestSpreadsheetColumnCountZero()
-        {
-            Spreadsheet s = new Spreadsheet(5, 0);
-            Assert.That(s.ColumnCount(), Is.EqualTo(0), "Spreadsheet returned unexpected column count.");
-        }
-
-        /// <summary>
-        /// Normal case for Spreadsheet RowCount method.
-        /// </summary>
-        [Test]
-        public void TestSpreadsheetRowCountNormal()
-        {
-            Spreadsheet s = new Spreadsheet(5, 6);
-            Assert.That(s.RowCount(), Is.EqualTo(5), "Spreadsheet returned unexpected row count.");
-        }
-
-        /// <summary>
-        /// Zero case for Spreadsheet RowCount method.
-        /// </summary>
-        [Test]
-        public void TestSpreadsheetRowCountZero()
-        {
-            Spreadsheet s = new Spreadsheet(0, 5);
-            Assert.That(s.RowCount(), Is.EqualTo(0), "Spreadsheet returned unexpected row count.");
+            Assert.That(tree.Evaluate(), Is.EqualTo(4.0), "Evaluate retuned unexpected value.");
         }
 
         //---------------------------------------------------------------
-        //              Expression Tree Evaluate Tests:
+        //                      Evaluate Tests:
         //---------------------------------------------------------------
 
         /// <summary>
@@ -214,53 +196,6 @@ namespace Tests_Spreadsheet_Nate_Gibson
             ExpressionTree tree = new ExpressionTree(expression);
 
             Assert.That(tree.Evaluate(), Is.EqualTo(expectedValue), "Evaluate retuned unexpected value.");
-        }
-
-        //---------------------------------------------------------------
-        //              Expression Tree SetVariable Tests:
-        //---------------------------------------------------------------
-
-        /// <summary>
-        /// CONDITION: Expression Tree Evaluate method works.
-        /// Tests a normal case for expression tree set variable method.
-        /// </summary>
-        [Test]
-        public void TestSetVariableNormal()
-        {
-            string expression = "A+1";
-            ExpressionTree tree = new ExpressionTree(expression);
-            tree.SetVariable("A", 3);
-
-            Assert.That(tree.Evaluate(), Is.EqualTo(4.0), "Evaluate retuned unexpected value.");
-        }
-
-        /// <summary>
-        /// CONDITION: Expression Tree Evaluate method works.
-        /// Tests a multiple variables case for expression tree set variable method.
-        /// </summary>
-        [Test]
-        public void TestSetVariableMultiple()
-        {
-            string expression = "A+1+B";
-            ExpressionTree tree = new ExpressionTree(expression);
-            tree.SetVariable("A", 3);
-            tree.SetVariable("B", 5);
-
-            Assert.That(tree.Evaluate(), Is.EqualTo(9.0), "Evaluate retuned unexpected value.");
-        }
-
-        /// <summary>
-        /// CONDITION: Expression Tree Evaluate method works.
-        /// Tests a long variable name case for expression tree set variable method.
-        /// </summary>
-        [Test]
-        public void TestSetVariableLongName()
-        {
-            string expression = "IAmThree+1";
-            ExpressionTree tree = new ExpressionTree(expression);
-            tree.SetVariable("IAmThree", 3);
-
-            Assert.That(tree.Evaluate(), Is.EqualTo(4.0), "Evaluate retuned unexpected value.");
         }
     }
 }
---
+++ Spreadsheet_Nate_Gibson/Tests_Spreadsheet_Nate_Gibson/SpreadsheetTests.cs
@@ -0,0 +1,80 @@
+﻿// Name: Nate Gibson
+// WSU ID: 11697165
+
+// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using CptS321;
+using NUnit.Framework;
+using SpreadsheetEngine;
+
+namespace Tests_Spreadsheet_Nate_Gibson
+{
+    /// <summary>
+    /// Test class for Spreadsheet project.
+    /// </summary>
+    public class SpreadsheetTests
+    {
+        /// <summary>
+        /// Normal case for Spreadsheet GetCell method.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetGetCellNormal()
+        {
+            Spreadsheet s = new Spreadsheet(5, 5);
+            SpreadsheetCell newCell = new SpreadsheetCell(0, 0);
+            Assert.That(s.GetCell(4, 0).Value, Is.EqualTo(newCell.Value), "Spreadsheet returned undexpected cell at index.");
+        }
+
+        /// <summary>
+        /// Nonexistent case for Spreadsheet GetCell method.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetGetCellNonexistent()
+        {
+            Spreadsheet s = new Spreadsheet(5, 5);
+            Assert.That(s.GetCell(6, 0), Is.EqualTo(null), "Spreadsheet returned undexpected cell at index.");
+        }
+
+        /// <summary>
+        /// Normal case for Spreadsheet ColumnCount method.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetColumnCountNormal()
+        {
+            Spreadsheet s = new Spreadsheet(6, 5);
+            Assert.That(s.ColumnCount(), Is.EqualTo(5), "Spreadsheet returned unexpected column count.");
+        }
+
+        /// <summary>
+        /// Zero case for Spreadsheet ColumnCount method.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetColumnCountZero()
+        {
+            Spreadsheet s = new Spreadsheet(5, 0);
+            Assert.That(s.ColumnCount(), Is.EqualTo(0), "Spreadsheet returned unexpected column count.");
+        }
+
+        /// <summary>
+        /// Normal case for Spreadsheet RowCount method.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetRowCountNormal()
+        {
+            Spreadsheet s = new Spreadsheet(5, 6);
+            Assert.That(s.RowCount(), Is.EqualTo(5), "Spreadsheet returned unexpected row count.");
+        }
+
+        /// <summary>
+        /// Zero case for Spreadsheet RowCount method.
+        /// </summary>
+        [Test]
+        public void TestSpreadsheetRowCountZero()
+        {
+            Spreadsheet s = new Spreadsheet(0, 5);
+            Assert.That(s.RowCount(), Is.EqualTo(0), "Spreadsheet returned unexpected row count.");
+        }
+    }
+}
